1f35a8e9461af31c1da7fb87737c5b97e1486759
Ubuntu <ubuntu@ip-10-202-34-94.ec2.internal>
2012-07-07 16:03:05 +0000

    Removed CommerceBug

--- web/app/code/local/Daviddaly/Commercebug/Block/Tab/Welcome.php
+++
@@ -1,8 +0,0 @@
-<?php
-	class Daviddaly_Commercebug_Block_Tab_Welcome extends Daviddaly_Commercebug_Block_Html
-	{
-		public function __construct()
-		{						
-			$this->setTemplate('tabs/ascommercebug_welcome.phtml');
-		}		
-	}
